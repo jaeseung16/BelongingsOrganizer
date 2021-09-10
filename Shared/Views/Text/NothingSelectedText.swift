92fdb6ee764e4b7d141ea84e0e1d99822d69b656
Jae-Seung Lee <jaeseung@gmail.com>
2021-09-10 14:40:22 -0400

    Refactor: Add NothingSelectedText

---
+++ Shared/Views/Text/NothingSelectedText.swift
@@ -0,0 +1,23 @@
+//
+//  NotSelectedView.swift
+//  Belongings Organizer
+//
+//  Created by Jae Seung Lee on 9/10/21.
+//
+
+import SwiftUI
+
+struct NothingSelectedText: View {
+    static let notSelected = "Nothing Selected"
+    
+    var body: some View {
+        Text(NothingSelectedText.notSelected)
+            .foregroundColor(.secondary)
+    }
+}
+
+struct NotSelectedView_Previews: PreviewProvider {
+    static var previews: some View {
+        NothingSelectedText()
+    }
+}
