d67b1ccfc138ef8f3b84a21a570c7456d23ef531
Jae-Seung Lee <jaeseung@gmail.com>
2021-09-10 14:40:45 -0400

    Refactor: Add SheetBottom

---
+++ Shared/Views/SheetBottom.swift
@@ -0,0 +1,37 @@
+//
+//  SheetBottom.swift
+//  Belongings Organizer
+//
+//  Created by Jae Seung Lee on 9/10/21.
+//
+
+import SwiftUI
+
+struct SheetBottom: View {
+    var labelText: String
+    
+    var labelAction: () -> Void
+    var done: () -> Void
+    
+    var body: some View {
+        HStack {
+            Button(action: labelAction) {
+                Label(labelText, systemImage: "plus")
+            }
+            
+            Spacer()
+            
+            Button(action: done) {
+                Text("Done")
+            }
+        }
+    }
+}
+
+struct SheetBottom_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetBottom(labelText: "Add a seller",
+                    labelAction: {},
+                    done: {})
+    }
+}
