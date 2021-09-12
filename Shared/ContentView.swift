8058b43da6d1dc9b8e8c1981ee1ac11e4d02b33b
Jae-Seung Lee <jaeseung@gmail.com>
2021-09-12 15:42:04 -0400

    Feature: Convert to TabView

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -17,74 +17,42 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
 
-    @State var presentAddBelongingView = false
-
     var body: some View {
-        NavigationView {
-            GeometryReader { geometry in
-                List {
-                    ForEach(items) { item in
-                        VStack {
-                            Text("\(item.name ?? "")")
-                            Text("price: \(item.currency ?? "") \(item.buyPrice)")
-                            Text("obtained: \(item.obtained ?? Date(), formatter: dateFormatter)")
-                            Text("added: \(item.created!, formatter: dateFormatter)")
-                        }
-                        
-                    }
-                    .onDelete(perform: deleteBelongings)
+        TabView {
+            ItemListView()
+                .environment(\.managedObjectContext, viewContext)
+                .environmentObject(viewModel)
+                .tabItem {
+                    Image(systemName: "gift.fill")
+                    Text("Items")
                 }
-                .sheet(isPresented: $presentAddBelongingView, content: {
-                    AddItemView(geometry: geometry)
-                        .environment(\.managedObjectContext, viewContext)
-                        .environmentObject(viewModel)
-                        .frame(minWidth: 350, minHeight: 450)
-                        .padding()
-                })
-                .toolbar {
-                    #if os(iOS)
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        EditButton()
-                    }
-                    #endif
-                    
-                    ToolbarItem(placement: .navigation) {
-                        Button(action: {
-                            presentAddBelongingView = true
-                        }) {
-                            Label("Add Belongings", systemImage: "plus")
-                        }
-                    }
+            
+            KindListView()
+                .environment(\.managedObjectContext, viewContext)
+                .environmentObject(viewModel)
+                .tabItem {
+                    Image(systemName: "c.square.fill")
+                    Text("Categories")
                 }
-                
-            }
-            .navigationTitle(Text("Belongings"))
-        }
-        
-    }
-
-    private func deleteBelongings(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            
+            ManufacturerListView()
+                .environment(\.managedObjectContext, viewContext)
+                .environmentObject(viewModel)
+                .tabItem {
+                    Image(systemName: "m.square.fill")
+                    Text("Manufacturers")
+                }
+            
+            SellerListView()
+                .environment(\.managedObjectContext, viewContext)
+                .environmentObject(viewModel)
+                .tabItem {
+                    Image(systemName: "shippingbox.fill")
+                    Text("Sellers")
+                }
         }
     }
 }
-
-private let dateFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
