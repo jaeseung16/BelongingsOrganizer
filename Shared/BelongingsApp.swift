cc03dfc4ff34d86729baa0f7bb7e6838a1e2df5b
Jae-Seung Lee <jaeseung@gmail.com>
2021-10-02 19:26:40 -0400

    Feature: Limit size in macOS

--- Shared/BelongingsApp.swift
+++ Shared/BelongingsApp.swift
@@ -14,9 +14,18 @@ struct BelongingsApp: App {
 
     var body: some Scene {
         WindowGroup {
+            #if os(macOS)
+            ContentView()
+                .frame(minWidth: 900, minHeight: 600)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(viewModel)
+            #else
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .environmentObject(viewModel)
+            #endif
+            
+            
         }
     }
 }
